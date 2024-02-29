1b400ef93db29b18742ad28dbc3e2c4cb3cc0178
devenamulshikder <devenamulshikder@gmail.com>
2024-02-29 13:57:52 +0600

    all phone handle one by one

--- index.js
+++ index.js
@@ -4,6 +4,37 @@ const loadData = async () => {
   );
   const data = await res.json();
   const phones = data.data;
-  console.log(phones);
+  // console.log(phones);
+  displayPhone(phones);
 };
-loadData()
+
+const displayPhone = (phones) => {
+  // step-1 
+  const phoneContainer = document.getElementById("phone-container");
+  phones.forEach(phones => {
+    console.log(phones);
+    // step-2 create a div
+    const phoneCard = document.createElement("div");
+    phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`;
+    // step-3 set inner html
+    phoneCard.innerHTML = `<figure class="px-10 pt-10">
+        <img
+          src="${phones.image}"
+          alt="Phones"
+          class="rounded-xl"
+        />
+      </figure>
+      <div class="card-body items-center text-center">
+        <h2 class="card-title">${phones.phone_name
+        }</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions">
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>`;
+      phoneContainer.appendChild(phoneCard)
+    // step-4 appendChild
+
+  });
+};
+loadData();
